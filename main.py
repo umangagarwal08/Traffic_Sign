66c231b812625e77441a6cbd87ae67f29651d743
Umang Agarwal <59832370+umangagarwal08@users.noreply.github.com>
2022-06-22 23:05:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import numpy as np
+import tensorflow as tf
 import matplotlib as plt
 import os
 
@@ -43,7 +44,55 @@ for i in range(classes):
         except:
             print("Error loading image")
 
+
 # Converting lists into numpy arrays
 data = np.array(data)
 labels = np.array(labels)
+
+#data splitting and conversion
+
+# Checking data shape
+print(data.shape, labels.shape)
+
+# Splitting training and testing dataset
+X_train, X_test, y_train, y_test = train_test_split(data, labels, test_size=0.2, random_state=42)
+
+# Displaying the shape after the split
+print(X_train.shape, X_test.shape, y_train.shape, y_test.shape)
+
+# Converting the labels into one hot encoding
+y_train = to_categorical(y_train, 43)
+y_test = to_categorical(y_test, 43)
+
+
+
+
+#creating and compiling Model
+
+model = Sequential()
+model.add(Conv2D(filters=32, kernel_size=(5,5), activation='relu', input_shape=X_train.shape[1:]))
+model.add(Conv2D(filters=32, kernel_size=(5,5), activation='relu'))
+model.add(MaxPool2D(pool_size=(2, 2)))
+model.add(Dropout(rate=0.25))
+model.add(Conv2D(filters=64, kernel_size=(3, 3), activation='relu'))
+model.add(Conv2D(filters=64, kernel_size=(3, 3), activation='relu'))
+model.add(MaxPool2D(pool_size=(2, 2)))
+model.add(Dropout(rate=0.25))
+model.add(Flatten())
+model.add(Dense(256, activation='relu'))
+model.add(Dropout(rate=0.5))
+model.add(Dense(43, activation='softmax'))
+
+# Compilation of the model
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+#M odel display
+model.summary()
+
+
+
+
+with tf.device('/GPU:0'):
+    epochs = 15
+    history1 = model.fit(X_train, y_train, batch_size=32, epochs=epochs, validation_data=(X_test, y_test))
 
